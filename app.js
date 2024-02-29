f1cc7d3bb62dc4fe494a3549eedf9b350296c349
shashikant010 <shashikantyadav0503@gmail.com>
2024-02-29 16:11:36 +0530

    trying to fix a issue

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ function voice(){
     recognition.lang="en-GB";
     recognition.onresult=(evt)=>{
         console.log(evt);
+        document.getElementById("speechtotext").value = evt.results[0][0].transcript;
     }
     recognition.start();
 }
+voice();
 but.addEventListener("click",voice);
